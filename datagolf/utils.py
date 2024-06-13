6144bacca1ae0d329ddad192fea65d68b0da287c
nscaamano <nscaamano@gmail.com>
2024-06-13 00:12:28 -0400

    Updated example for tiered preds to work with new code.

--- datagolf/utils.py
+++ datagolf/utils.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import json
 import csv
-from typing import List
+from typing import List, Dict
 
 
 def open_json_file(filename: str) -> dict:
@@ -13,9 +13,13 @@ def open_json_file(filename: str) -> dict:
 def write_json_file(filename: str, data: dict) -> None:
     pass
 
-
-def write_dict_data_to_csv(filename: str, data: List[dict]) -> None: 
-    with open(os.path.join(Path(os.path.abspath(__file__)).parent.parent.absolute(), filename,), 'w', newline='') as csvfile:
+def write_dict_data_to_csv(filename: str, data: List[Dict]) -> None:
+    file_path = Path(os.path.abspath(__file__)).parent.parent.absolute() / filename
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    with open(file_path, 'w', newline='') as csvfile:
+        if not data:
+            return
         field_names = data[0].keys()
         writer = csv.DictWriter(csvfile, fieldnames=field_names)
         writer.writeheader()
--- examples/tiered_pre_tournamant_preds.py
+++ examples/tiered_pre_tournamant_preds.py
@@ -3,19 +3,22 @@ from lookups.pga_champ_2024 import TIERS
 from datagolf.models import PreTournamentPredModel
 from datagolf.utils import write_dict_data_to_csv
 
+#  note add to env
+#  export PYTHONPATH=/path/to/py-datagolf
+
 
 PRED_MODEL = 'baseline_history_fit'
+TOURNAMENT_PREFIX = 'usopen'
 
 
 if __name__ == '__main__':
     api = DgAPI()
     
-    preds = api._request.pre_tournament_predictions()  # todo replace with api once supported
-    #tier_1_ids = [player.dg_id for player in api.common.get_players(names=TIER_1)]
+    preds = api._request.pre_tournament_predictions()  # TODO replace with api once supported
     output = {}
     
     for tier, values in TIERS.items():
-        dg_ids = [player.dg_id for player in api.get_players(name=values)]
+        dg_ids = [player.dg_id for player in api.get_players(player_name=values)]
         output_preds = []
         for pred in preds[PRED_MODEL]:  # once supported by api use . notation
             if pred['dg_id'] in dg_ids:
@@ -23,6 +26,6 @@ if __name__ == '__main__':
         output[tier] = output_preds
         
     for tier, data in output.items():
-        write_dict_data_to_csv(filename=f'examples/output/pre_tournament_preds_pga_{tier.lower()}.csv', data=data)
+        write_dict_data_to_csv(filename=f'examples/output/pre_tournament_preds_{TOURNAMENT_PREFIX}_{tier.lower()}.csv', data=data)
 
         
